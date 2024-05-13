6108ae53e94cb9306d675e4a45e79699dd8d8a06
Artur Chaves <artur7paiva@gmail.com>
2024-05-13 00:50:35 -0300

    Grafico animado e jogo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from funcoes import *
-
+from game import *
 x_robo = float(input("Digite a posição em X do robô: "))
 y_robo = float(input("Digite a posição em Y do robô: "))
 
@@ -11,7 +11,8 @@ while(True):
 3. Gráfico dos componentes vx e vy da velocidade da bola e do robô em função do tempo t  
 4. Gráfico dos componentes ax e ay da aceleração da bola e do robô em função do tempo t
 5. Gráfico da distância relativa d entre o robô e a bola como função do tempo t
-6. Sair.
+6. Gerar animação da simulação
+7. Sair.
           """)
 
     opcao = int(input("Digite a opção desejada: "))
@@ -27,6 +28,8 @@ while(True):
     elif opcao == 5:
         distancia_robo_bola()
     elif opcao == 6:
+        animar()
+    elif opcao == 7:
         break
     else:
         print("Opção inválida, tente novamente!")
